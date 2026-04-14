3340bc9d3cd2ff6a472b50356619ad512040f821
littlewangzai-orz <littlewangzai-orz>
2026-04-14 10:43:05 +0800

    16

--- exercises/16_mysed/16_mysed.c
+++ exercises/16_mysed/16_mysed.c
@@ -8,16 +8,34 @@ int parse_replace_command(const char* cmd, char** old_str, char** new_str) {
     if (cmd[0] != 's' || cmd[1] != '/') {
         return -1;
     }
-
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
 
+    char buf[256];
+    strcpy(buf, cmd + 2);
+
+    char *token = strtok(buf, "/");
+    *old_str = (char*)malloc(strlen(token) + 1);
+    strcpy(*old_str, token);
+
+    token = strtok(NULL, "/");
+    *new_str = (char*)malloc(strlen(token) + 1);
+    strcpy(*new_str, token);
     return 0;
 }
 
 void replace_first_occurrence(char* str, const char* old, const char* new) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char *pos;
+    char buf[256];
+
+    if( (pos = strstr(str, old)) != NULL) {
+        size_t prefix = pos - str;
+        strncpy(buf, str, prefix);
+        strcpy(buf + prefix, new);
+        strcpy(buf + prefix + strlen(new), pos + strlen(old));
+        strcpy(str, buf);
+    }
+
 }
 
 int main(int argc, char* argv[]) {
