b84715946ef4efab2cd175bf2a2fdeb196c395af
littlewangzai-orz <littlewangzai-orz>
2026-04-14 16:26:46 +0800

    21

--- exercises/21_singly_linked_list_josephus/21_singly_linked_list_josephus.c
+++ exercises/21_singly_linked_list_josephus/21_singly_linked_list_josephus.c
@@ -66,16 +66,27 @@ void josephus_problem(int n, int k, int m) {
         if (m == 1) {
             // m==1 时当前节点直接出列
             // TODO: 在这里添加你的代码
-            // I AM NOT DONE
+            printf("%d ", current->item);
+            link next = next_wrap(current);
+            delete(current);
+            current = next;
         }
 
         // 数到 m 的那个人出列：从 current 开始走 m-1 步，落在第 m 个节点
         // TODO: 在这里添加你的代码
-        // I AM NOT DONE
+        else {
+            for(int i = 0; i < m-1; i ++) {
+                current = next_wrap(current);
+            }
+            // 此时 current 指向要出列的人
+            // TODO: 在这里添加你的代码
+            printf("%d ", current->item);
+            link next = next_wrap(current);
+            delete(current);
+            current = next;
+        }
 
-        // 此时 current 指向要出列的人
-        // TODO: 在这里添加你的代码
-        // I AM NOT DONE
+        
     }
 
     printf("\n");
--- exercises/21_singly_linked_list_josephus/singly_linked_list.c
+++ exercises/21_singly_linked_list_josephus/singly_linked_list.c
@@ -8,8 +8,13 @@ static link head = NULL;
 
 // 创建新节点
 link make_node(unsigned char item) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link new_node = malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        return NULL;
+    }
+    new_node->item = item;
+    new_node->next = NULL;
+    return new_node;
 }
 
 // 释放节点
@@ -17,48 +22,93 @@ void free_node(link p) { free(p); }
 
 // 查找节点
 link search(unsigned char key) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link cur = head;
+    while (cur != NULL) {
+        if (cur->item == key) {
+            return cur;
+        }
+        cur = cur->next;
+    }
+
+    return NULL;
 }
 
 // 在链表头部插入节点
 void insert(link p) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (p == NULL) {
+        return;
+    }
+    p->next = head;
+    head = p;
 }
 
 // 删除指定节点
 void delete(link p) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (head == NULL || p == NULL) {
+        return;
+    }
+    link cur = head;
+    if (p == head) {
+        head = head->next;
+        free_node(p);
+        return;
+    } else {
+        while (cur != NULL && cur->next != NULL) {
+            if (cur->next == p) {
+                cur->next = cur->next->next;
+                free_node(p);
+                return;
+            }
+            cur = cur->next;
+        }
+    }
 }
 
 // 遍历链表
 void traverse(void (*visit)(link)) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (visit == NULL) {
+        return;
+    }
+    link cur = head;
+    while (cur != NULL) {
+        visit(cur);
+        cur = cur->next;
+    }
 }
 
 // 销毁整个链表
 void destroy(void) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    free_list(head);
+    head = NULL;
 }
 
 // 在链表头部推入节点
-void push(link p) { 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
- }
+void push(link p) {
+    if (p == NULL) {
+        return;
+    }
+    p->next = head;
+    head = p;
+}
 
 // 从链表头部弹出节点
 link pop(void) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (head == NULL) {
+        return NULL;
+    }
+
+    link head_node = head;
+    head = head->next;
+    head_node->next = NULL;
+    return head_node;
 }
 
 // 释放链表内存
 void free_list(link list_head) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    link cur = list_head;
+    while (cur != NULL) {
+        link next = cur->next;
+        free_node(cur);
+        cur = next;
+    }
 }
